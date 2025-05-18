d8584442eac71c27311cde8043fa0f2633291bff
srupesh0711 <sadirupeshr@gmail.com>
2025-05-18 13:27:52 +0530

    random commit

--- exam1/src/main/java/com/exam1/Exam1Application.java
+++ exam1/src/main/java/com/exam1/Exam1Application.java
@@ -8,6 +8,8 @@ public class Exam1Application {
 		int a = 10;
 		int b =20;
 		int c = 30;
+		double d = 12.5f;
+		boolean r = true;
 	public static void main(String[] args){
 		SpringApplication.run(Exam1Application.class, args);
 	}
