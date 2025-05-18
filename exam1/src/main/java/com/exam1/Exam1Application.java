4e7f98ab09e820dcfd2d36638b43f2253a0e2287
S_RUPESH _REDDY <sadirupeshr@gmail.com>
2025-05-18 13:17:10 +0530

    Update Exam1Application.java

--- exam1/src/main/java/com/exam1/Exam1Application.java
+++ exam1/src/main/java/com/exam1/Exam1Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Exam1Application {
-
+		int a = 10;
+		int b =20;
+		int c = 30;
 	public static void main(String[] args){
 		SpringApplication.run(Exam1Application.class, args);
 	}
